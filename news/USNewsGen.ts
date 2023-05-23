37c9416992baf90e100623b702ba1cd56b8bd614
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2023-05-22 20:35:17 -0500

    Fixing small errors

--- news/USNewsGen.ts
+++ news/USNewsGen.ts
@@ -13,7 +13,7 @@ dotenv.config();
  * ```
  */
 async function main() {
-  var sumText;
+  var sumText = '';
   const api = new BingChat({ cookie: '1gdAhVNcuprCC0teLdxNXc2SDzwhrCfgmfmXHp6vGU6LzJ9hXufW9KH0qHmdnSKj6hkjc_R2tjY7FlkxP7Cld1KAnFpZEEwdS1wWSh-DqEneYSHpgYs-7FgtaQKw6vd45htg0AJnJgFMoDwg-2HuvNIpwtSqjZIK4Qa_8uDvGVawWSloecY6oksaJ5dRc187IJp-OaGfa36hiVZHgBoeVY6VD6NCB9qs4UJ3HBqy4HAg' });
 
   const prompt = 'US news headlines from May 22, 2023';
@@ -37,7 +37,7 @@ async function main() {
     })
     console.log('\n' + res.text + '\n')
     let phrase = 'I\'m sorry but I couldn\'t find'
-    if (!(str.includes(phrase))) {
+    if (!(res.text.includes(phrase))) {
       sumText += res.text;
     }
   }
