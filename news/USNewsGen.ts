82992d1b2112036e67eb0dd9ae11b568461ea7ec
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2023-05-22 20:13:04 -0500

    Basic news generator

--- news/USNewsGen.ts
+++ news/USNewsGen.ts
@@ -1,51 +1,43 @@
-import dotenv from 'dotenv-safe'
-import { oraPromise } from 'ora'
+import dotenv from 'dotenv-safe';
+import { oraPromise } from 'ora';
 
-import { BingChat } from '../src'
+import { BingChat } from '../src';
 
-dotenv.config()
+dotenv.config();
 
 /**
- * Demo CLI for testing conversation support.
+ * CLI for generating US News Headlines.
  *
  * ```
  * npx tsx news/USNewsGen.ts
  * ```
  */
 async function main() {
-  const api = new BingChat({ cookie: "1c-6nx5dn9TomP2rkgpljthk3u9BxVh_KKXMOV0LzefQ_-zTfTUbj9y5s5t3m1WNrwKAf7HMOOrDQmqJ6iLHNfejIJcBrJoR817Rmd7CSx5CAjWsaDkf9zawK5MI5xhG1WN7-k9BXpDYEEq_ke2xOozSHJPTr8PKLTZYsjDLwFa6UbD5aX6hPj_bDH8LW1hHJnb01utmNtLX-ZbPjtRjMn_Cu3egDnkypoTmvhPZpxEw" })
+  const api = new BingChat({ cookie: '1gdAhVNcuprCC0teLdxNXc2SDzwhrCfgmfmXHp6vGU6LzJ9hXufW9KH0qHmdnSKj6hkjc_R2tjY7FlkxP7Cld1KAnFpZEEwdS1wWSh-DqEneYSHpgYs-7FgtaQKw6vd45htg0AJnJgFMoDwg-2HuvNIpwtSqjZIK4Qa_8uDvGVawWSloecY6oksaJ5dRc187IJp-OaGfa36hiVZHgBoeVY6VD6NCB9qs4UJ3HBqy4HAg' });
 
-  const prompt = 'Write a poem about cats.'
+  const prompt = 'US news headlines from May 22, 2023';
 
   let res = await oraPromise(api.sendMessage(prompt), {
     text: prompt
   })
 
-  console.log('\n' + res.text + '\n')
+  console.log('\n' + res.text + '\n');
 
-  const prompt2 = 'Can you make it cuter and shorter?'
+  var stories = res.text.split('-');
+  stories.shift();
+  console.log(stories);
 
-  res = await oraPromise(api.sendMessage(prompt2, res), {
-    text: prompt2
-  })
-  console.log('\n' + res.text + '\n')
+  for (let story of stories) {
+    const prompt2 = `Can you expand on ${story}?`;
 
-  const prompt3 = 'Now write it in French.'
-
-  res = await oraPromise(api.sendMessage(prompt3, res), {
-    text: prompt3
-  })
-  console.log('\n' + res.text + '\n')
-
-  const prompt4 = 'What were we talking about again?'
-
-  res = await oraPromise(api.sendMessage(prompt4, res), {
-    text: prompt4
-  })
-  console.log('\n' + res.text + '\n')
+    res = await oraPromise(api.sendMessage(prompt2, res), {
+      text: prompt2
+    })
+    console.log('\n' + res.text + '\n')
+  }
 }
 
 main().catch((err) => {
-  console.error(err)
-  process.exit(1)
+  console.error(err);
+  process.exit(1);
 })
